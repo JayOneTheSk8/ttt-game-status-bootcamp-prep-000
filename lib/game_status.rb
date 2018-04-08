37b6ba33be42ac884591d896a0fdbe64851aa13d
Justin Cox <misterjustincox@gmail.com>
2018-04-08 13:56:43 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -46,11 +46,11 @@ def full?(board)
 end
 
 def draw?(board)
-  if won(board) != false
-    return false
-  elsif !won?(board) && full?(board) == true
+  if won?(board) == false && full?(board) == true
     return true
-  elsif !won?(board) && full?(board) == false
+  elsif won?(board) == false && full?(board) == false
+    return false
+  elsif !won?(board)
     return false
   end
 end
